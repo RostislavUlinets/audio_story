83f0e6bfde9c551df25e8680afd9a610f82f0325
Rostislav <poct2002@gmail.com>
2022-01-12 10:29:56 +0200

    [NEW] Icons class

--- audio_story/lib/resources/app_colors.dart
+++ audio_story/lib/resources/app_colors.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 
-class AppColors{
+class AppColors {
+  const AppColors._();
 
-  AppColors._();
-  
-  static  const Color green = Color(0xFF71A59F);
-  static  const Color orange =  Color(0xFFF1B488);
-  static  const Color purpule =  Color(0xFF8C84E2);
-  static  const Color blue =  Color(0xFF678BD2);
-  static  const Color red =  Color(0xFFE27777);
-  static  const Color black =  Color(0xFF3A3A55);
+  static const Color green = Color(0xFF71A59F);
+  static const Color orange = Color(0xFFF1B488);
+  static const Color purpule = Color(0xFF8C84E2);
+  static const Color blue = Color(0xFF678BD2);
+  static const Color red = Color(0xFFE27777);
+  static const Color black = Color(0xFF3A3A55);
 }
---
+++ audio_story/lib/resources/app_icons.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class MyIconsClass {
+  const MyIconsClass._();
+
+  static Image defaultAvatar = Image.asset('assets/anon_ava.jpg');
+  static Image arrowDown = Image.asset('assets/Arrow - Down.png');
+  static Image arrowDownInCircle =
+      Image.asset('assets/Arrow - Left Circle.png');
+  static Image arrowLeftInCircle = Image.asset('assets/ArrowBack.png');
+  static Image camera = Image.asset('assets/Camera.png');
+  static Image category = Image.asset('assets/Category.png');
+  static Image cloudUpload = Image.asset('assets/CloudUpload.png');
+  static Image delete = Image.asset('assets/Delete.png');
+  static Image edit = Image.asset('assets/Edit.png');
+  static Image cycleArrow = Image.asset('assets/fluent_arrow.png');
+  static Image heart = Image.asset('assets/Heart.png');
+  static Image home = Image.asset('assets/Home.png');
+  static Image infinity = Image.asset('assets/Infinity.png');
+  static Image paper = Image.asset('assets/Paper.png');
+  static Image paperDownload = Image.asset('assets/PaperDownload.png');
+  static Image pause = Image.asset('assets/Pause.png');
+  static Image play = Image.asset('assets/Play.png');
+  static Image playBack = Image.asset('assets/PlayBack.png');
+  static Image playFoward = Image.asset('assets/PlayFront.png');
+  static Image playRec = Image.asset('assets/PlayRec.png');
+  static Image profile = Image.asset('assets/Profile.png');
+  static Image plus = Image.asset('assets/PlusIcon.png');
+  static Image record = Image.asset('assets/RecordIcon.png');
+  static Image search = Image.asset('assets/Search.png');
+  static Image swap = Image.asset('assets/Swap.png');
+  static Image complite = Image.asset('assets/TickSquare.png');
+  static Image upload = Image.asset('assets/Upload.png');
+  static Image voice = Image.asset('assets/Voice.png');
+  static Image wallet = Image.asset('assets/Wallet.png');
+}
